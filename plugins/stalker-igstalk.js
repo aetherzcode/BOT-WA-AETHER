edbf23918803988786f41afdb66f9504c1cf32a5
AETHER <aetherzcode@gmail.com>
2024-11-17 02:17:02 +0700

    Update stalker-igstalk.js

--- plugins/stalker-igstalk.js
+++ plugins/stalker-igstalk.js
@@ -1,21 +1,23 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `contoh:\n${usedPrefix + command} erlanrahmat_14`
+    if (!text) throw `contoh:\n${usedPrefix + command} deaafrizal`
     try {
-        let api = await fetch(`https://api.betabotz.eu.org/api/stalk/ig?username=${text}&apikey=${lann}`)
+        let api = await fetch(`https://api.betabotz.eu.org/api/stalk/yt?username=${text}&apikey=${lann}`)
         let response = await api.json()
         if (response.status) {
-            let { photoUrl, postsCount, followers, following, bio, fullName, username } = response.result;
+            let { channelId, url, channelName, avatar, isVerified, subscriberH, subscriber, description } = response.result.data[0];
             let capt;
-            capt = `乂 *I G S T A L K E R*\n\n`;
-            capt += `◦ *Username* : ${username}\n`;
-            capt += `◦ *Full Name* : ${fullName}\n`;
-            capt += `◦ *Bio* : ${bio}\n`;
-            capt += `◦ *Followers* : ${followers}\n`;           
-            capt += `◦ *Following* : ${following}\n`;            
-            capt += `◦ *Total Post* : ${postsCount}\n`;
-           capt += `\n`;        
-            return conn.sendFile(m.chat, photoUrl, 'pp.png', capt, m)
+            capt = `乂 *Y O U T U B E  C H A N N E L*\n\n`;
+            capt += `◦ *Channel ID* : ${channelId}\n`;
+            capt += `◦ *URL* : ${url}\n`;
+            capt += `◦ *Channel Name* : ${channelName}\n`;
+            capt += `◦ *Avatar* : ${avatar}\n`;
+            capt += `◦ *Verified* : ${isVerified}\n`;
+            capt += `◦ *Subscribers* : ${subscriberH}\n`;
+            capt += `◦ *Subscriber Count* : ${subscriber}\n`;
+            capt += `◦ *Description* : ${description}\n`;
+            capt += `\n`; 
+            return conn.sendFile(m.chat, avatar, 'pp.png', capt, m)
         } else {
             throw 'Sistem Sedang Bermasalah!'
         }
@@ -24,9 +26,9 @@ let handler = async (m, { text, usedPrefix, command }) => {
     }
 }
 
-handler.help = ['igstalk <username>']
+handler.help = ['ytstalk <username>']
 handler.tags = ['stalk']
-handler.command = /^(igstalk)$/i
+handler.command = /^(ytstalk)$/i
 handler.limit = true
 
 module.exports = handler
