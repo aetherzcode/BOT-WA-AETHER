cd2fbff46122f9d346a047e6fa0cbdf33afb6840
AETHER <aetherzcode@gmail.com>
2024-12-04 19:01:00 +0700

    Upload file _veikone-ai.js

---
+++ plugins/_veikone-ai.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+ if(!text) throw `Masukkan query yang benar\nContoh ${usedPrefix}${command} selamat pagi`;
+ try {
+ let prompt = "Kamu adalah VeikoneAI, Kamu mempunyai sifat Tsundere"; //Ubah Sendiri Sesukamu
+ let veiku = await fetch(`https://api.nyxs.pw/ai/character-ai?prompt=${text}&gaya=${prompt}`);
+ let output = await veiku.json();
+ await m.reply(output.result);
+ } catch (e) {
+  throw e;
+ }
+}
+
+handler.command = ["vei"];
+handler.tags = ["ai"];
+handler.help = ["vei *<text>*"];
+handler.limit = true;
+module.exports = handler;
