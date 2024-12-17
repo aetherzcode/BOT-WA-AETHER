a689db5afb3f22de956a164e46bd9f1152ad03aa
AETHER <aetherzcode@gmail.com>
2024-12-17 10:57:34 +0700

    Update tools-tomp3.js

--- plugins/tools-tomp3.js
+++ plugins/tools-tomp3.js
@@ -13,5 +13,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['toaudio (reply)']
 handler.tags = ['tools']
 handler.command = /^to(a(udio)?)$/i
+handler.register = true
 
 module.exports = handler
