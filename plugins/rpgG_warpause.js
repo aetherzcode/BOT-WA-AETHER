83395c28cc19cbbae47c100aa931e52ad7dadcf8
AETHER <aetherzcode@gmail.com>
2024-12-20 08:20:45 +0700

    Update rpgG_warpause.js

--- plugins/rpgG_warpause.js
+++ plugins/rpgG_warpause.js
@@ -18,4 +18,5 @@ let handler = async (m, { conn }) => {
 handler.help = ['guildwarpause'];
 handler.tags = ['rpgG'];
 handler.command = /^(guildwarpause)$/i;
+handler.register = true;
 module.exports = handler;
