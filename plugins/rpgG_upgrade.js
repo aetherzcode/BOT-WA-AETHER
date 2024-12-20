ed17d707431498a4cc67c1573bb0fa19de8ebd52
AETHER <aetherzcode@gmail.com>
2024-12-20 08:22:52 +0700

    Update rpgG_upgrade.js

--- plugins/rpgG_upgrade.js
+++ plugins/rpgG_upgrade.js
@@ -58,4 +58,5 @@ let handler = async (m, { conn, args }) => {
 handler.help = ['guildupgrade <level/eliksir/harta/guardian/attack>'];
 handler.tags = ['rpgG'];
 handler.command = /^(guildupgrade)$/i;
+handler.register = true;
 module.exports = handler;
